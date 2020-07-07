1e4ad3aa6184a60d3b0d65ae655b03f4b0163059
Amanda Irazabal <amanda.irazabal@gmail.com>
2020-07-07 16:28:33 -0400

    Added MongoStore

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const mongoose = require("mongoose");
 var session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 var passport = require("passport");
 
 app.use(express.urlencoded({ extended: true }));
@@ -15,10 +16,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ieat");
+
 // Express Session
 app.use(
   session({
     secret: "secret",
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
     saveUninitialized: true,
     resave: true
   })
@@ -31,7 +35,7 @@ app.use(passport.session());
 const apiRoutes = require("./routes/apiRoutes");
 app.use(apiRoutes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ieat");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/ieat");
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
